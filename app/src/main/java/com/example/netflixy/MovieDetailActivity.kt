5ccf2ea34c52c3a67c45f3448bc06536b9b1c29b
Rafael de Melo Silva <rafamsilva@live.com>
2019-12-23 17:02:01 -0300

    icon and back button

--- app/src/main/java/com/example/netflixy/MovieDetailActivity.kt
+++ app/src/main/java/com/example/netflixy/MovieDetailActivity.kt
@@ -27,13 +27,13 @@ class MovieDetailActivity : AppCompatActivity() {
         val call = RetrofitInitializer().apiService().getMovieDetail(imdbID = imdbID)
         call.enqueue(object : Callback<MovieDetail> {
             override fun onResponse(call: Call<MovieDetail>, response: Response<MovieDetail>) {
-                response?.body()?.let {
+                response.body()?.let {
                     bindView(it)
                 }
             }
 
             override fun onFailure(call: Call<MovieDetail>, t: Throwable) {
-                Log.d("eoq", "deu ruim")
+                d("eoq", "deu ruim")
             }
         })
     }
@@ -44,5 +44,24 @@ class MovieDetailActivity : AppCompatActivity() {
         movie_detail_imdb.text = movie.imdbRating.toString()
         movie_detail_runtime.text = movie.runtime
         movie_detail_year.text = movie.year.toString()
+        movie_detail_production.text = "Produtora: ${movie.production}"
+        movie_detail_description.text = movie.description
+        movie_detail_actors.text = "Atores: ${movie.actors}"
+        movie_detail_director.text = "Diretores: ${movie.director}"
+        movie_detail_language.text = "Idiomas: ${movie.language}"
+        movie_detail_genre.text = "Gêneros: ${movie.genre}"
+
+        setActionBar(movie.title)
+    }
+
+    private fun setActionBar(title : String) {
+        val actionbar = supportActionBar
+        actionbar!!.title = title
+        actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
